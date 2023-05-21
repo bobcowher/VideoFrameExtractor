9428328a5cda493f7178ca34967ee4bcec56ee95
Robert Cowher <bobcowher@gmail.com>
2023-05-20 20:04:23 -0500

    Initial Image Extractor Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+A short program to use CV2 to convert a video into individual frames
+This is a great way to get lots of images for AI training.
+
+Version 1.0.0
---
+++ main.py
@@ -0,0 +1,50 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+import cv2
+import os
+
+filename = sys.argv[1]
+
+divider = filename.find(".")
+
+name = filename[:divider]
+
+
+print(f"Processing {filename}")
+
+video = cv2.VideoCapture(filename)
+
+fps = video.get(cv2.CAP_PROP_FPS)
+print(f'Frames per second = {fps}')
+
+frame_number = 0
+
+output_dir = f"{name}_image_output"
+
+# Ensure the directory structure exists.
+try:
+    os.mkdir(output_dir)
+except:
+    print(f"Directory {output_dir} already exists or could not be created")
+
+# Loop through and process images.
+try:
+    while True:
+        video.set(cv2.CAP_PROP_POS_MSEC, frame_number)
+        ret, frame = video.read()
+        cv2.imwrite(f'./{name}_image_output/{name}_frame_{frame_number}.png', frame)
+
+        if frame_number % 1000 == 0:
+            print(f"Processed {frame_number} images...")
+
+        frame_number += 1
+
+except:
+    print(f"Completed writing {frame_number} images")
+
+
+
